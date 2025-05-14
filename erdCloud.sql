9628b8c47fc58435af5dad7e6fdc5722df8da124
gaheeyoon <a5312559@gmail.com>
2025-05-14 12:42:01 +0900

    erdCloud.sql

--- erdCloud.sql
+++ erdCloud.sql
@@ -1,41 +1,34 @@
-﻿CREATE TABLE `member` (
-	`memberId`	char(100)	NOT NULL,
-	`pw`	char(10)	NULL,
-	`name`	varchar(100)	NOT NULL	COMMENT '회원이름',
-	`tel`	varchar(100)	NULL
-);
--- 
-CREATE TABLE `buy` (
-	`Key`	int	NOT NULL,
-	`memberId2`	char(100)	NOT NULL,
-	`productName`	varchar(100)	NULL,
-	`price`	int	NULL
+ 
+CREATE TABLE `member` (
+	`memberID`	varchar(8)	NOT NULL	COMMENT '회원id는 최대 8글자까지',
+	`name`	varchar(255)	NULL	COMMENT '회원이름은 한글기준으로 255글자',
+	`tel`	varchar(20)	NULL	COMMENT '연락처는 전화번호/집번호 다 허용함.'
 );
 
 CREATE TABLE `board` (
 	`boardId`	int	NOT NULL	COMMENT '게시판id',
+	`memberID`	varchar(8)	NOT NULL	COMMENT '회원id는 최대 8글자까지',
 	`title`	varchar(100)	NULL,
-	`content`	text	NULL,
-	`memberId`	char(100)	NOT NULL
+	`content`	text	NULL
 );
 
 ALTER TABLE `member` ADD CONSTRAINT `PK_MEMBER` PRIMARY KEY (
-	`memberId`
-);
-
-ALTER TABLE `buy` ADD CONSTRAINT `PK_BUY` PRIMARY KEY (
-	`Key`,
-	`memberId2`
+	`memberID`
 );
 
 ALTER TABLE `board` ADD CONSTRAINT `PK_BOARD` PRIMARY KEY (
-	`boardId`
+	`boardId`,
+	`memberID`
 );
 
-ALTER TABLE `buy` ADD CONSTRAINT `FK_member_TO_buy_1` FOREIGN KEY (
-	`memberId2`
+ALTER TABLE `board` ADD CONSTRAINT `FK_member_TO_board_1` FOREIGN KEY (
+	`memberID`
 )
 REFERENCES `member` (
-	`memberId`
+	`memberID`
 );
+
+
+
+
 
